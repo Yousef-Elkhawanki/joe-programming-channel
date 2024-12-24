8f9224fd6f32cf68193c51beaf8605652d3fbfb0
Yousef-Elkhawanki <dev.elkhawanki@gmail.com>
2024-12-24 04:19:45 +0300

    update code

--- react-bootstrap/src/Box.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import "./box.css"
-export default function Box({ children, auth }) {
-    return auth ?<div className="box">{children}</div>:<h1>Unauthnicated</h1>;
-}
--- react-bootstrap/src/ProductsList.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function ProductsList({ product_name, hideName }) {
-    return !hideName && <li>product name is :{product_name}</li>;
-}
